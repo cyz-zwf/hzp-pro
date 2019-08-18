0dfa3f91b6bc664600b10fd78cccf1c8237ea536
cyz <cyz@flower.com>
2019-08-18 15:44:43 +0800

    cyz

---
+++ hzp-vue/src/components/Top.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bg">
+    <div class="top">
+      <div class="top lt font_small">
+        <ul>
+          <li>
+            <a href="#">欢迎来到化妆品店</a>
+          </li>
+          <li>
+            <a href="login.html">请登录</a>
+          </li>
+          <li>
+            <a href="#">免费注册</a>
+          </li>
+          <li>
+            <a href="#">常见问题</a>
+          </li>
+        </ul>
+      </div>
+      <span></span>
+      <div class="top rt font_small">
+        <ul>
+          <li>
+            <a href="#">帮助中心</a>
+          </li>
+          <li>
+            <span class="img1">
+              <img src="http://127.0.0.1:5050/img/header/mobile-sephora.png" alt />
+            </span>
+            <a href="#">手机收藏</a>
+          </li>
+          <li>
+            <span class="img1">
+              <img src="http://127.0.0.1:5050/img/header/vip-club.png" alt />
+            </span>
+            <a href="#">会员俱乐部</a>
+          </li>
+          <li>
+            <span class="img1">
+              <img src="http://127.0.0.1:5050/img/header/my-sephora.png" alt />
+            </span>
+            <a class="mr-0" href="#">我的购物车</a>
+            <div class="tubiao"></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    @import url("../assets/css/comm.css");
+    @import url("../assets/css/top.css");
+</style>
--- hzp-vue/src/main.js
+++ hzp-vue/src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios" 
+import  Vuex from "vuex"
+import Top from "./components/Top"
 
-Vue.config.productionTip = false
+//保证所有vue都可以使用axios
+Vue.prototype.axios=axios
+//项目中请求服务器路径配置
+axios.defaults.baseURL="http:127.0.0.1:5050"
+//保证session对象不丢失
+axios.defaults.withCredentials=true
+//注册vuex
+Vue.use(Vuex)
+Vue.component("top",Top);
+
+Vue.config.productionTip = false 
 
 new Vue({
   router,
